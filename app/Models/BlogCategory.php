0908409c96a9cd021539d4c862860e152850d43f
dmitriy <da_belousov@list.ru>
2021-04-21 17:38:37 +0300

    Отношение parentCategory, создали accessor getParentTitleAttribute

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -6,10 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ *Class BlogCategory
+ *
+ * @package App\Models
+ *
+ * @property-read BlogCategory $parentCategory
+ * @property-read string $parentTitle
+ */
+
 class BlogCategory extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    const ROOT_ID = 1;
 
     protected $fillable = [
         'title',
@@ -17,4 +28,42 @@ class BlogCategory extends Model
         'parent_id',
         'description',
     ];
+
+    /**
+     *Получить родительскую категорию
+     *
+     * @return BlogCategory
+     */
+    public function parentCategory()
+    {
+        return $this->belongsTo(BlogCategory::class, 'parent_id', 'id');
+    }
+
+    /**
+     *Пример аксесуара (Accessor)
+     *
+     *@return string
+     */
+    public function getParentTitleAttribute()
+    {
+
+        $title = $this->parentCategory->title
+            ?? ($this->isRoot()
+                ? 'Корень'
+                : '???');
+
+        return $title;
+    }
+
+    /**
+     *Является ли текущий объект корнем
+     *
+     * @return bool
+     */
+    private function isRoot()
+    {
+        return $this->id === BlogCategory::ROOT_ID;
+    }
+
+
 }
